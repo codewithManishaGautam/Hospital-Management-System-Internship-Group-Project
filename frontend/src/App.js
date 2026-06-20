62f2958b4ae24fb7c8df31a8e04f07572d103df6
Prajwal Kale <kaleprajwal0612@gmail.com>
2026-06-20 12:09:49 +0530

    updated project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,31 @@
 import React from "react";
 import {
-  BrowserRouter,
   Routes,
   Route,
+  BrowserRouter,
 } from "react-router-dom";
 
-import LoginDashboard from "./pages/Login";
-import ReceptionistDashboard from "./pages/Receptionist";
-import DoctorDashboard from "./pages/Doctor";
-import LabDashboard from "./pages/Lab";
-import PharmacyDashboard from "./pages/Pharmacy";
-import NurseDashboard from "./pages/Nurse";
-import BillingDashboard from "./pages/Billing";
-import InsuranceDashboard from "./pages/Insurance";
-import AdminDashboard from "./pages/Admin";
+// import LoginDashboard from "./pages/Login";
+// import ReceptionistDashboard from "./pages/Receptionist";
+// import DoctorDashboard from "./pages/Doctor";
+// import LabDashboard from "./pages/Lab";
+// import PharmacyDashboard from "./pages/Pharmacy";
+// import NurseDashboard from "./pages/Nurse";
+// import BillingDashboard from "./pages/Billing";
+// import InsuranceDashboard from "./pages/Insurance";
+// import AdminDashboard from "./pages/Admin";
 
 
 
 
-// import BillingDept from "./components/Billing_Module/BillingDept";
-// import PatientDetail from "./components/Billing_Module/PatientDetail";
+import BillingDept from "./Components/Billing_Module/BillingDept";
+import PatientDetail from "./Components/Billing_Module/PatientDetail";
 // import ReceptionDept from "./components/ReceptionDept";
 // import SignaturePad from "./components/SignaturePad";
 
 // import AddDiagnostic
 // from "./components/Diagnostic_Module/AddDiagnostic";
 // import PICCEng from "./components/ConcernForm_Module/Routine_Consents/PICC/PICCEng";
-
 
 
 
@@ -39,88 +38,94 @@ function App() {
     // <AddDiagnostic />
 
 
-
-    //   <Routes>
-
-    //     <Route
-    //       path="/"
-    //       element={<BillingDept>
-
-    //       </BillingDept>}
-    //     />
-
-        
-
-    //     <Route
-    //       path="/patient/:id"
-    //       element={<PatientDetail />}
-    //     />
-
-    //   </Routes>
-
-
-
-
     <BrowserRouter>
 
       <Routes>
 
-        {/* Login Page */}
         <Route
           path="/"
-          element={<LoginDashboard />}
+          element={<BillingDept>
+
+          </BillingDept>}
         />
 
-        {/* Receptionist */}
-        <Route
-          path="/receptionist"
-          element={<ReceptionistDashboard />}
-        />
 
-        {/* Doctor */}
-        <Route
-          path="/doctor"
-          element={<DoctorDashboard />}
-        />
 
-        {/* Lab Module */}
         <Route
-          path="/lab"
-          element={<LabDashboard />}
-        />
-
-        {/* Pharmacy */}
-        <Route
-          path="/pharmacy"
-          element={<PharmacyDashboard />}
-        />
-
-        {/* Nurse */}
-        <Route
-          path="/nurse"
-          element={<NurseDashboard />}
-        />
-
-        {/* Billing */}
-        <Route
-          path="/billing"
-          element={<BillingDashboard />}
-        />
-
-        {/* Insurance */}
-        <Route
-          path="/insurance"
-          element={<InsuranceDashboard />}
-        />
-
-        {/* Admin */}
-        <Route
-          path="/admin"
-          element={<AdminDashboard />}
+          path="/patient/:id"
+          element={<PatientDetail />}
         />
 
       </Routes>
+
     </BrowserRouter>
+
+
+
+
+    // <BrowserRouter>
+
+    //   <Routes>
+
+    //     {/* Login Page */}
+    //     <Route
+    //       path="/"
+    //       element={<LoginDashboard />}
+    //     />
+
+    //     {/* Receptionist */}
+    //     <Route
+    //       path="/receptionist"
+    //       element={<ReceptionistDashboard />}
+    //     />
+
+    //     {/* Doctor */}
+    //     <Route
+    //       path="/doctor"
+    //       element={<DoctorDashboard />}
+    //     />
+
+    //     {/* Lab Module */}
+    //     <Route
+    //       path="/lab"
+    //       element={<LabDashboard />}
+    //     />
+
+    //     {/* Pharmacy */}
+    //     <Route
+    //       path="/pharmacy"
+    //       element={<PharmacyDashboard />}
+    //     />
+
+    //     {/* Nurse */}
+    //     <Route
+    //       path="/nurse"
+    //       element={<NurseDashboard />}
+    //     />
+
+    //     {/* Billing */}
+    //     <Route
+    //       path="/billing"
+    //       element={<BillingDashboard />}
+    //     />
+
+    //     {/* Insurance */}
+    //     <Route
+    //       path="/insurance"
+    //       element={<InsuranceDashboard />}
+    //     />
+
+    //     {/* Admin */}
+    //     <Route
+    //       path="/admin"
+    //       element={<AdminDashboard />}
+    //     />
+
+    //   </Routes>
+    // </BrowserRouter>
+
+
+
   );
 }
 
--- frontend/src/Components/Billing_Module/BillingDept.jsx
+++ frontend/src/Components/Billing_Module/BillingDept.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './css/BillingDept.css';
+// import "./css/BillingDept.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PdfCreate from './PdfCreate';
 import { faBars, faBell } from "@fortawesome/free-solid-svg-icons";
@@ -12,14 +12,14 @@ function BillingDept() {
     return (
         <div >
             <h1>Billing Department</h1>
-            <nav className="nav">
+            <nav className="navbar">
                 <FontAwesomeIcon icon={faBars} style={{ margin: "5px 0 0 0" }} />
                 <input placeholder='Patient name' className="form-control" style={{width:"300px"}}></input>
-                <p class="position-relative">
+                <p className="position-relative">
                     <FontAwesomeIcon icon={faBell} style={{ margin: "5px 0 0 0", height: "40px", width: "30px" }} />
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ margin: "5px 0 0 0", height: "auto" }}>
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ margin: "5px 0 0 0", height: "auto" }}>
                         99+
-                        <span class="visually-hidden">unread messages</span>
+                        <span className="visually-hidden">unread messages</span>
                     </span>
                 </p>
                 <b>
