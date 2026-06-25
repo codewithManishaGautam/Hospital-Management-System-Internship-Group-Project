18ef3b2a9735cb3519e4dc547dd4a4f7c3e5ef08
Manisha Gautam <YOUR_GITHUB_EMAIL>
2026-06-26 00:39:28 +0530

    Update doctor backend routes and controller

--- Backend/routes/doctorRoutes.js
+++ Backend/routes/doctorRoutes.js
@@ -2,7 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const { addDoctorPrescription } = require("../controllers/doctorController");
+const { 
+  addDoctorPrescription,
+  getAllDoctors,
+  getDoctorById
+} = require("../controllers/doctorController");
+
+// Get all doctors
+router.get("/doctors", getAllDoctors);
+
+// Get single doctor by ID
+router.get("/doctors/:doctorId", getDoctorById);
 
 // Doctor prescription save
 router.post("/doctor/prescriptions", addDoctorPrescription);
