29446350ba4e59c4b75e98f31f2bf42ff79c0968
Sakshi Kasbe <sakshikasbe291@gmail.com>
2026-06-30 20:05:31 +0530

    Update App.js routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,35 @@ import NurseDashboard from "./pages/Nurse";
 import BillingDashboard from "./pages/Billing";
 import InsuranceDashboard from "./pages/Insurance";
 import AdminDashboard from "./pages/Admin";
+
 import VerifyAccount from "./Components/Login/VerifyAccount";
 import ForgotPassword from "./Components/Login/ForgotPassword";
 import ResetPassword from "./Components/Login/ResetPassword";
 import Register from "./Components/Login/Register";
 
-// import BillingDept from "./components/Billing_Module/BillingDept";
-// import PatientDetail from "./components/Billing_Module/PatientDetail";
+import VenousEngMar from "./Components/ConcernForm_Module/Routine_Consents/CentralVenousCatheterisation/VenousEngMar";
+
+// import {
+//   Routes,
+//   Route,
+//   BrowserRouter,
+// } from "react-router-dom";
+
+// import LoginDashboard from "./pages/Login";
+// import ReceptionistDashboard from "./pages/Receptionist";
+// import DoctorDashboard from "./pages/Doctor";
+// import LabDashboard from "./pages/Lab";
+// import PharmacyDashboard from "./pages/Pharmacy";
+// import NurseDashboard from "./pages/Nurse";
+// import BillingDashboard from "./pages/Billing";
+// import InsuranceDashboard from "./pages/Insurance";
+// import AdminDashboard from "./pages/Admin";
+
+// >>>>>>> 21a693d2d1562609078baee245cf2c5eadb5b2ff
+
+// import BillingDept from "./Components/Billing_Module/BillingDept";
+// import PatientDetail from "./Components/Billing_Module/PatientDetail";
+
 // import ReceptionDept from "./components/ReceptionDept";
 // import SignaturePad from "./components/SignaturePad";
 
@@ -27,6 +49,10 @@ import Register from "./Components/Login/Register";
 function App() {
   return (
     // <AddDiagnostic />
+
+    // <VenousEngMar/>
+
+    // <BrowserRouter>
 
     //   <Routes>
 
@@ -82,6 +108,68 @@ function App() {
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
+    // </BrowserRouter>
+
+    // <BrowserRouter>
+
+    //   <Routes>
+
+    //     {/* Login Page */}
+    //     <Route
+    //       path="/"
+    //       element={<LoginDashboard />}
+    //     />
+
+    //     {/* Receptionist */}
+    //     <Route
+    //       path="/receptionist"
+    //       element={<ReceptionistDashboard />}
+    //     />
+
+    //     {/* Doctor */}
+    //     <Route
+    //       path="/doctor"
+    //       element={<DoctorDashboard />}
+    //     />
+
+    //     {/* Lab Module */}
+    //     <Route
+    //       path="/lab"
+    //       element={<LabDashboard />}
+    //     />
+
+    //     {/* Pharmacy */}
+    //     <Route
+    //       path="/pharmacy"
+    //       element={<PharmacyDashboard />}
+    //     />
+
+    //     {/* Nurse */}
+    //     <Route
+    //       path="/nurse"
+    //       element={<NurseDashboard />}
+    //     />
+
+    //     {/* Billing */}
+    //     <Route
+    //       path="/billing"
+    //       element={<BillingDashboard />}
+    //     />
+
+    //     {/* Insurance */}
+    //     <Route
+    //       path="/insurance"
+    //       element={<InsuranceDashboard />}
+    //     />
+
+    //     {/* Admin */}
+    //     <Route
+    //       path="/admin"
+    //       element={<AdminDashboard />}
+    //     />
+
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
